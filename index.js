1bc9faa49d947daf86d7106529d78220a1190e5e
tillarnold <throwable42@gmail.com>
2014-10-13 20:29:41 +0200

    Move `CanvasEventEmitter` to its own file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/canvas-utils');
+var general = require('./lib/canvas-utils.js');
+var CanvasEventEmitter = require('./lib/canvasEventEmitter.js');
+
+
+
+module.exports.CanvasEventEmitter = CanvasEventEmitter;
+module.exports.convertEventCoords = general.convertEventCoords;
+module.exports.rotateContextAt = general.rotateContextAt;
