7053f036070f7c48f8494013cc513d29e12da849
tillarnold <throwable42@gmail.com>
2014-10-13 22:38:05 +0200

    Add `degreesToRadians` and `radiansToDegrees`

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ var CanvasEventEmitter = require('./lib/canvasEventEmitter.js');
 
 
 module.exports.CanvasEventEmitter = CanvasEventEmitter;
+
 module.exports.convertEventCoords = general.convertEventCoords;
 module.exports.rotateContextAt = general.rotateContextAt;
+
+module.exports.radiansToDegrees = general.radiansToDegrees;
+module.exports.degreesToRadians = general.degreesToRadians;
