ec01c7a55193b5a51b4f8a3a6f952dc3717d387f
Till Arnold <throwable42@gmail.com>
2015-04-07 13:50:21 +0000

    Format code

--- src-es6/lib/canvas-utils.js
+++ src-es6/lib/canvas-utils.js
@@ -1,57 +1,52 @@
-/**
- * @param {MouseEvent} e - DOM event
- * @param {HTMLCanvasElement} canvas - HTML canvas element
- *
- * @return {Object} the converted coordinate as object {x,y}
- */
-export let convertEventCoords = function convertEventCoords(e, canvas) {
-  let rect = canvas.getBoundingClientRect()
-    , x = e.pageX - rect.left - document.body.scrollLeft
-    , y = e.pageY - rect.top - document.body.scrollTop
-    , computed = window.getComputedStyle(canvas)
-
-  x *= canvas.width / parseInt(computed.width, 10)
-  y *= canvas.height / parseInt(computed.height, 10)
-
-  return { x, y }
-},
-
-
-/**
- * @param {CanvasRenderingContext2D} ctx - The context that should be rotated
- * @param {Number} x - coordinate
- * @param {Number} y - coordinate
- * @param {Number} r - Rotation in radians
- *
- * @return {CanvasRenderingContext2D} return the context passed in (for chaining)
- */
-rotateContextAt = function rotateContextAt(ctx, x, y, r) {
-  ctx.translate(x, y)
-  ctx.rotate(r)
-  ctx.translate(-1 * x, -1 * y)
-  return ctx
-},
+export
+    /**
+     * @param {MouseEvent} e - DOM event
+     * @param {HTMLCanvasElement} canvas - HTML canvas element
+     *
+     * @return {Object} the converted coordinate as object {x,y}
+     */
+let convertEventCoords = function convertEventCoords(e, canvas) {
+      let rect = canvas.getBoundingClientRect()
+        , x = e.pageX - rect.left - document.body.scrollLeft
+        , y = e.pageY - rect.top - document.body.scrollTop
+        , computed = window.getComputedStyle(canvas)
+      x *= canvas.width / parseInt(computed.width, 10)
+      y *= canvas.height / parseInt(computed.height, 10)
+      return { x, y }
+    }
 
 
+    /**
+     * @param {CanvasRenderingContext2D} ctx - The context that should be rotated
+     * @param {Number} x - coordinate
+     * @param {Number} y - coordinate
+     * @param {Number} r - Rotation in radians
+     *
+     * @return {CanvasRenderingContext2D} return the context passed in (for chaining)
+     */
+  , rotateContextAt = function rotateContextAt(ctx, x, y, r) {
+      ctx.translate(x, y)
+      ctx.rotate(r)
+      ctx.translate(-1 * x, -1 * y)
+      return ctx
+    }
 
-/**
- * Convert radians to degrees
- *
- * @param {Number} r - Angle in radians
- *
- * @return {Number} Angle in degree
- */
-radiansToDegrees = function radiansToDegrees(r) {
-  return r * 180 / Math.PI
-},
 
-/**
- * Convert degrees to radians
- *
- * @param {Number} d - Angle in degrees
- *
- * @return {Number} Angle in radian
- */
-degreesToRadians = function degreesToRadians(d) {
-  return d * Math.PI / 180
-}
+
+    /**
+     * Convert radians to degrees
+     *
+     * @param {Number} r - Angle in radians
+     *
+     * @return {Number} Angle in degree
+     */
+  , radiansToDegrees = r => r * 180 / Math.PI
+
+    /**
+     * Convert degrees to radians
+     *
+     * @param {Number} d - Angle in degrees
+     *
+     * @return {Number} Angle in radian
+     */
+  , degreesToRadians = d => d * Math.PI / 180
--- src-es6/lib/canvasEventEmitter.js
+++ src-es6/lib/canvasEventEmitter.js
@@ -7,116 +7,110 @@ import { convertEventCoords } from './canvas-utils.js'
  * @param {Element} eventSource - defaults to window.document
  */
 const createCanvasEventEmitter = function createCanvasEventEmitter(target, eventSource = window.document) {
+  let isIn = false
   const emitter = new EventEmitter()
-  let isIn = false,
+        /**
+         * Get the coordinates of an event relative to
+         * the target element
+         */
+      , getCoords = event => convertEventCoords(event, target)
+
+        /**
+         * Calculate if the coordinates (as given by getCoords)
+         * are on the canvas
+         */
+      , areCoordsOnCanvas = function areCoordsOnCanvas(xy) {
+          const { x, y } = xy
+              , { width, height } = target
+
+          if (x < 0 || y < 0) {
+            return false
+          }
+
+          if (x > width || y > height) {
+            return false
+          }
+
+          return true
+        }
+
+        /**
+         * Wraps the DOM event into a CanvasEventEmitter event and emits it.
+         * @param {Event} event - the DOM event
+         * @param {Object} xy - the coordinate to emit
+         * @param {string} overrideType - optional defaults to event.type
+         */
+      , emitCanvasEventRaw = function emitCanvasEventRaw(event, xy, type = event.type) {
+
+          const { x, y } = xy
+
+          emitter.emit(type, { x
+                             , y
+                             , target
+                             , event
+                             , button: event.button
+                             , preventDefault: () => event.preventDefault()
+                             })
+        }
 
 
+        /**
+         * Emits the given DOM Event if the coordinates are on the canvas
+         * @param {Event} event
+         */
+      , emitCanvasEventIfOnCanvas = function emitCanvasEventIfOnCanvas(event, overrideType) {
+          const xy = getCoords(event)
+
+          if (!areCoordsOnCanvas(xy)) {
+            return
+          }
+
+          emitCanvasEventRaw(event, xy, overrideType)
+        }
+
+        /**
+         * Attaches the DOM listeners
+         * Should only be called once
+         */
+      , initListeners = function initListeners() {
+          //add the basic listeners to the dom
+          ['click', 'mousedown', 'mouseup'].forEach( eventType => {
+            eventSource.addEventListener(eventType, emitCanvasEventIfOnCanvas)
+          })
 
 
-  /**
-   * Get the coordinates of an event relative to
-   * the target element
-   */
-  getCoords = function getCoords(event) {
-    return convertEventCoords(event, target)
-  },
+          //Add listener for mousemove
+          //We calculate mouseover and mouseout from this
+          eventSource.addEventListener('mousemove', event => {
 
-  /**
-   * Calculate if the coordinates (as given by getCoords)
-   * are on the canvas
-   */
-  areCoordsOnCanvas = function areCoordsOnCanvas(xy) {
-    const { x, y } = xy
-        , { width, height } = target
+            const xy = getCoords(event)
+                , onCanvas = areCoordsOnCanvas(xy)
 
-    if (x < 0 || y < 0) {
-      return false
-    }
-
-    if (x > width || y > height) {
-      return false
-    }
-
-    return true
-  },
-
-  /**
-   * Wraps the DOM event into a CanvasEventEmitter event and emits it.
-   * @param {Event} event - the DOM event
-   * @param {Object} xy - the coordinate to emit
-   * @param {string} overrideType - optional defaults to event.type
-   */
-  emitCanvasEventRaw = function emitCanvasEventRaw(event, xy, type = event.type) {
-
-    const { x, y } = xy
-
-    emitter.emit(type, { x
-                       , y
-                       , target
-                       , event
-                       , button: event.button
-                       , preventDefault: () => event.preventDefault()
-                       })
-  },
+            if (onCanvas) {
+              //Emit the standard mousemove event
+              emitCanvasEventRaw(event, xy)
+            }
 
 
-  /**
-   * Emits the given DOM Event if the coordinates are on the canvas
-   * @param {Event} event
-   */
-  emitCanvasEventIfOnCanvas = function emitCanvasEventIfOnCanvas(event, overrideType) {
-    const xy = getCoords(event)
+            //Emit mouseover and mouseout
+            if (onCanvas && !isIn) {
+              isIn = true
+              emitCanvasEventRaw(event, xy, 'mouseover')
+            }
 
-    if (!areCoordsOnCanvas(xy)) {
-      return
-    }
+            if (!onCanvas && isIn) {
+              isIn = false
+              emitCanvasEventRaw(event, xy, 'mouseout')
+            }
 
-    emitCanvasEventRaw(event, xy, overrideType)
-  },
+          })
 
-  /**
-   * Attaches the DOM listeners
-   * Should only be called once
-   */
-  initListeners = function initListeners() {
-    //add the basic listeners to the dom
-    ['click', 'mousedown', 'mouseup'].forEach( eventType => {
-      eventSource.addEventListener(eventType, emitCanvasEventIfOnCanvas)
-    })
-
-
-    //Add listener for mousemove
-    //We calculate mouseover and mouseout from this
-    eventSource.addEventListener('mousemove', event => {
-
-      const xy = getCoords(event)
-          , onCanvas = areCoordsOnCanvas(xy)
-
-      if (onCanvas) {
-        //Emit the standard mousemove event
-        emitCanvasEventRaw(event, xy)
-      }
-
-
-      //Emit mouseover and mouseout
-      if (onCanvas && !isIn) {
-        isIn = true
-        emitCanvasEventRaw(event, xy, 'mouseover')
-      }
-
-      if (!onCanvas && isIn) {
-        isIn = false
-        emitCanvasEventRaw(event, xy, 'mouseout')
-      }
-
-    })
-
-    //Listener for contextmenu. also trigers the click event
-    eventSource.addEventListener('contextmenu', event => {
-      emitCanvasEventIfOnCanvas(event)
-      emitCanvasEventIfOnCanvas(event, 'click')
-    })
-  }
+          //Listener for contextmenu. also trigers the click event
+          eventSource.addEventListener('contextmenu', event => {
+            emitCanvasEventIfOnCanvas(event)
+            emitCanvasEventIfOnCanvas(event, 'click')
+          })
+        }
 
   initListeners()
   return emitter
